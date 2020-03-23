de92590b09e88bdaec3667125a10b42e9e31848f
ingagudrun <60742454+ingagudrun@users.noreply.github.com>
2020-03-23 11:08:39 +0000

    Update Passenger.java

--- Passenger.java
+++ Passenger.java
@@ -1,14 +1,22 @@
 public class Passenger {
     private String name;
-    private int SSN;
+    private int ssn;
     private String gender;
     private String email;
     private int phoneNumber;
     
+    public Passenger(String name, int ssn, String gender, String email, int phoneNumber){
+        this.name=name;
+        this.ssn=ssn;
+        this.gender=gender;
+        this.email=email;
+        this.phoneNumber=phoneNumber;
+    }
+    
     public String getName(){return name;}
     public void setName(String val){name=val;}
-    public int getSSN(){return SSN;}
-    public void setSSN(int val){SSN=val;}
+    public int getSsn(){return ssn;}
+    public void setSsn(int val){ssn=val;}
     public String getGender(){return gender;}
     public void setGender(String val){gender=val;}
     public String getEmail(){return email;}
