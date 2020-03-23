5caea6a93a2898c54d0499859ac3c5b0836c492d
ingagudrun <60742454+ingagudrun@users.noreply.github.com>
2020-03-23 09:43:05 +0000

    Create Passenger.java

---
+++ Passenger.java
@@ -0,0 +1,19 @@
+public class Passenger {
+    private String name;
+    private int SSN;
+    private String gender;
+    private String email;
+    private int phoneNumber;
+    
+    public String getName(){return name;}
+    public void setName(String val){name=val;}
+    public int getSSN(){return SSN;}
+    public void setSSN(int val){SSN=val;}
+    public String getGender(){return gender;}
+    public void setGender(String val){gender=val;}
+    public String getEmail(){return email;}
+    public void setEmail(String val){email=val;}
+    public int getPhoneNumber(){return phoneNumber;}
+    public void setPhoneNumber(int val){phoneNumber=val;}
+    
+}
