81503f59fc790571734e441199b1c6114bd510cb
ingagudrun <60742454+ingagudrun@users.noreply.github.com>
2020-03-23 09:46:02 +0000

    Create Seat.java

---
+++ Seat.java
@@ -0,0 +1,12 @@
+public class Seat {
+    private String seatNumber;
+    private boolean availability;
+    private int price;
+    
+    public String getSeatNumber(){return seatNumber;}
+    public void setSeatNumber(String val){seatNumber=val;}
+    public boolean getAvailability(){return availability;}
+    public void setAvailability(boolean val){availability=val;}
+    public int getPrice(){return price;}
+    public void setPrice(int val){price=val;}
+}
