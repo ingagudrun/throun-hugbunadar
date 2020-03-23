5ae9873ec4f90eff325de4f6ad538c2063c56921
ingagudrun <60742454+ingagudrun@users.noreply.github.com>
2020-03-23 11:05:30 +0000

    Update Flight.java

--- Flight.java
+++ Flight.java
@@ -1,9 +1,9 @@
 public class Flight {
 
-    private String FLIGHT_NUMBER;
-    private String AIRLINE;
-    private String DEST_TO;
-    private String DEST_FROM;
+    private String flightNumber;
+    private String airline;
+    private String destTo;
+    private String destFrom;
     private String departureTime;
     private String arrivalTime;
     private int price;
@@ -12,14 +12,24 @@ public class Flight {
     private Seat[] seats;
     private int luggagePrice;
     
-    public String getFlightNumber(){return FLIGHT_NUMBER;}
-    public void setFlightNumber(String val){FLIGHT_NUMBER=val;}
-    public String getAirline(){return AIRLINE;}
-    public void setAirline(String val){AIRLINE=val;}
-    public String getDestTo(){return DEST_TO;}
-    public void setDestTo(String val){DEST_TO=val;}
-    public String getDestFrom(){return DEST_FROM;}
-    public void setDestFrom(String val){DEST_FROM=val;}
+    public Flight(String flightNumber, String airline, String destTo, String destFrom, String departureTime, String arrivalTime, int price){
+        this.flightNumber = flightNumber;
+        this.airline = airline;
+        this.destFrom = destFrom;
+        this.destTo = destTo;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+        this.price = price;
+    }
+    
+    public String getFlightNumber(){return flightNumber;}
+    public void setFlightNumber(String val){flightNumber=val;}
+    public String getAirline(){return airline;}
+    public void setAirline(String val){airline=val;}
+    public String getDestTo(){return destTo;}
+    public void setDestTo(String val){destTo=val;}
+    public String getDestFrom(){return destFrom;}
+    public void setDestFrom(String val){destFrom=val;}
     public String getDepartureTime(){return departureTime;}
     public void setDepartureTime(String val){departureTime=val;} 
     public String getArrivalTime(){return arrivalTime;}
@@ -43,15 +53,8 @@ public class Flight {
         /**
      * Updates the availability of a certain seat
      * ATH: þurfum að gera þannig að hverju sæti samsvari boolean breyta sem segir
-     * hvort það sé available og þá laga í þessu hvernig við gerum það
+     * hvort það sé available
      */
     public void updateAvailability(Seat seat, boolean available){
-        for(Seat i in seats){
-            if(i==seat)
-                
-        }
-        //kannski gera þetta öðruvísi, td athuga fyrst hvort sætið breytist ekki í raun
-        if(available) setAvailableSeats(availableSeats++);
-        else if(!available) setAvailableSeats(availableSeats--);
     
     }
