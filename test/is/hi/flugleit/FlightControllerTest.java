e5876cc3fe975e6f5dc2b5394e7c3baccaa822e7
Inga Guðrún Eiríksdóttir <60742454+ingagudrun@users.noreply.github.com>
2020-03-25 17:52:10 +0000

    Update FlightControllerTest.java

--- test/is/hi/flugleit/FlightControllerTest.java
+++ test/is/hi/flugleit/FlightControllerTest.java
@@ -8,14 +8,32 @@ public class FlightControllerTest {
   
   @Before
   public void setUp() { 
-    
+    flights=new Flight[2];
+    flights[0]= new Flight("A515", "Air Iceland Connect", "Akureyri", "Reykjavík", "27.03.2020-18:45", "27.03.2020-19:30", 15000);
+    flights[1]= new Flight("A573", "Air Iceland Connect", "Akureyri", "Reykjavík", "28.03.2020-07:00", "28.03.2020-07:50", 12500);
   }
   @After
   public void tearDown() { 
+    flights=null;
+    //ATH: þarf að gera eftirfarandi eða er nóg að gera bara hitt?
+    flights[0]=null;
+    flights[1]=null;
   }
   @Test
-  public void testSomeBehavior() {
+  public void testSearch() {
   
   assert...(...);
+  }
+  
+  @Test
+  public void testSortByPrice() {
+    Flight[] flights2=flights.sort(price);
+    assertEquals(flights[1],flights2[0]);
+  }
+  
+  @Test
+  public void testSortByDuration() {
+    Flight[] flights2=flights.sort(duration);
+    assertEquals(flights[0],flights2[0]);
   }
 }
