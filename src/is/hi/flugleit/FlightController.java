dd34b51f4854d32bba9a8f7e08951f5ef31b7819
Brynjar Ingimarsson <brynjar@ingimarsson.is>
2020-03-25 13:50:29 +0000

    added directory structure

---
+++ src/is/hi/flugleit/FlightController.java
@@ -0,0 +1,5 @@
+package is.hi.flugleit;
+
+class FlightController {
+
+}
---
+++ src/is/hi/flugleit/Seat.java
@@ -0,0 +1,11 @@
+package is.hi.flugleit;
+
+class Seat {
+    private String seatNumber;
+    private int price;
+
+    public void Seat(String seatNumber, int price) {
+        this.seatNumber = seatNumber;
+        this.price = price;
+    }
+}
