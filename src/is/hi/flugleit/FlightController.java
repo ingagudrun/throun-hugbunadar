9567d251b64196282b461e5cee386783c28cd435
Inga Guðrún Eiríksdóttir <60742454+ingagudrun@users.noreply.github.com>
2020-03-25 17:25:51 +0000

    Update FlightController.java

--- src/is/hi/flugleit/FlightController.java
+++ src/is/hi/flugleit/FlightController.java
@@ -1,5 +1,24 @@
 package is.hi.flugleit;
 
-class FlightController {
+public class FlightController {
+  private Flight[] flights;
+  
+  public Flight[] search(String date, String destTo, String destFrom){
+  }
+  
+  public void sort(String sortProperty){
+    if(sortProperty.equals("date")||sortProperty.equals("Date")||sortProperty.equals("DATE"))
+      flights.sortByDate();
+    else if(sortProperty.equals("price")||sortProperty.equals("Price")||sortProperty.equals("PRICE"))
+      flights.sortByPrice();
+    else if(sortProperty.equals("duration")||sortProperty.equals("Duration")||sortProperty.equals("DURATION"))
+      flights.sortByDuration();
+    else if (sortProperty.equals("arrival time")||sortProperty.equals("Arrival time")||sortProperty.equals("ARRIVAL TIME"))
+      flights.sortByArrivalTime();
+  }
+  private void sortByDate(){}
+  private void sortByPrice(){}
+  private void sortByDuration(){}
+  private void sortByArrivalTime(){}
 
 }
