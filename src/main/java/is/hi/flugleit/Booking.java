f96bd68ca59e4036617eb3257696d4b8ffe7629e
Inga Guðrún Eiríksdóttir <60742454+ingagudrun@users.noreply.github.com>
2020-04-10 14:53:19 +0000

    Update Booking.java

--- src/main/java/is/hi/flugleit/Booking.java
+++ src/main/java/is/hi/flugleit/Booking.java
@@ -38,10 +38,13 @@ public class Booking {
         JsonObjectBuilder result = Json.createObjectBuilder();
 
         result.add("bookingNumber", this.getBookingNumber());
-        result.add("flight", this.getFlight());
-        result.add("seat", this.getSeat());
-        result.add("passenger", this.getPassenger());
-    
+        result.add("flight", this.getFlight().getJson);
+        result.add("seat", this.getSeat().getJson);
+        result.add("passenger", this.getPassenger.().getJson);
+        result.add("luggage", this.getLuggage());   
+        result.add("paid", this.getPaid());
+        result.add("refunded", this.getRefunded());
+       
         return result;
     }
 }
